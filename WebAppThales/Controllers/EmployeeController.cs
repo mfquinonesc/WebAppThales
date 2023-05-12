c18865b5505d59eec516d9404ceef45d32f50780
mfquinonesc <manuelfqc@gmail.com>
2023-05-12 10:35:54 -0500

    update 7

--- WebAppThales/Controllers/EmployeeController.cs
+++ WebAppThales/Controllers/EmployeeController.cs
@@ -13,6 +13,38 @@ namespace WebAppThales.Controllers
 {
     public class EmployeeController : Controller
     {
+        private const string API_EMPLOYEES = "http://dummy.restapiexample.com/api/v1/";
+
+        public EmployeeViewModel GetEmployee(int id)
+        {
+            EmployeeViewModel employee = new EmployeeViewModel();
+            try
+            {
+                using (var client = new HttpClient())
+                {
+                    client.BaseAddress = new Uri(API_EMPLOYEES);
+                    client.DefaultRequestHeaders.Accept.Clear();
+                    client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                    var responseTask = client.GetAsync($"employee/{id}");
+                    responseTask.Wait();
+                    var result = responseTask.Result;
+
+                    if (result.IsSuccessStatusCode)
+                    {
+                        string jsonResult = result.Content.ReadAsStringAsync().Result;
+                        var arrJSON = (JObject)JsonConvert.DeserializeObject(jsonResult);
+                        var data = arrJSON.GetValue("data");
+                        //employees = data.ToObject<List<EmployeeViewModel>>();
+                    }
+                }
+            }
+            catch(Exception ex)
+            {
+                employee = null;
+            }
+            return employee;
+        }
+
         public List<EmployeeViewModel> GetEmployeesList()
         {
             List<EmployeeViewModel> employees = new List<EmployeeViewModel>();
@@ -20,7 +52,7 @@ namespace WebAppThales.Controllers
             {
                 using (var client = new HttpClient())
                 {
-                    client.BaseAddress = new Uri("http://dummy.restapiexample.com/api/v1/");
+                    client.BaseAddress = new Uri(API_EMPLOYEES);
                     client.DefaultRequestHeaders.Accept.Clear();
                     client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
                     var responseTask = client.GetAsync("employees");
@@ -87,10 +119,11 @@ namespace WebAppThales.Controllers
 
         [HttpGet]
         public IActionResult Search(string Id)
-        {            
+        { 
             int id = 0;
             if (int.TryParse(Id, out id))
             {
+                this.GetEmployee(id);
                 return Redirect($"/Employee/Employee/{id}");
             }
             else
